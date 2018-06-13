6a918c34f4bcd7c70a0cabebf9658947a65d3100
Bencho Naut <benchonaut__AT__github.com@b1nch0.us.to>
2018-06-13 19:09:18 +0200

    2018-06-13_17:09 upgraded to https and auto-reenabling account

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 USERNAME = ""
 PASSWORD = ""
-URL = "http://freedns.afraid.org"
+URL = "https://freedns.afraid.org"
+URL_DORMANT = "https://freedns.afraid.org/dormant/"
+URL_DORMANT_EXTEND = "https://freedns.afraid.org/dormant/?action=extend&confirm=1"
